bb0990e1fa22416d0d34f0ef9b009b079a3afbc9
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-08-31 13:43:59 -0300

    Integração com o Organograma.

--- SCD/Integration/Organograma/Base/IOrganogramaService.cs
+++ SCD/Integration/Organograma/Base/IOrganogramaService.cs
@@ -1,10 +1,12 @@
 ﻿using Prodest.Scd.Integration.Organograma.Model;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Prodest.Scd.Integration.Organograma.Base
 {
     public interface IOrganogramaService
     {
+        Task<List<OrganogramaOrganizacao>> SearchAsync();
         Task<OrganogramaOrganizacao> SearchAsync(string guid);
         Task<OrganogramaOrganizacao> SearchPatriarcaAsync(string guid);
     }
