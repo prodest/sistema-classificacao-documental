e21a47272c31ed5593cfcc452a0535fa0873ee6c
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-09-04 15:05:27 -0300

    Trocar de branch

---
+++ SCD/Business/NivelClassificacaoCore.cs
@@ -0,0 +1,159 @@
+﻿using AutoMapper;
+using Prodest.Scd.Business.Base;
+using Prodest.Scd.Business.Model;
+using Prodest.Scd.Integration.Organograma.Base;
+using Prodest.Scd.Integration.Organograma.Model;
+using Prodest.Scd.Persistence.Base;
+using Prodest.Scd.Persistence.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodest.Scd.Business
+{
+    public class NivelClassificacaoCore : INivelClassificacaoCore
+    {
+        private IUnitOfWork _unitOfWork;
+        private IGenericRepository<NivelClassificacao> _nivelsClassificacao;
+        private NivelClassificacaoValidation _validation;
+        private IMapper _mapper;
+        private IOrganogramaService _organogramaService;
+        private IOrganizacaoCore _organizacaoCore;
+
+        public NivelClassificacaoCore(IScdRepositories repositories, NivelClassificacaoValidation validation, IMapper mapper, IOrganogramaService organogramaService, IOrganizacaoCore organizacaoCore)
+        {
+            _unitOfWork = repositories.UnitOfWork;
+            _nivelsClassificacao = repositories.NivelsClassificacao;
+            _validation = validation;
+            _mapper = mapper;
+            _organogramaService = organogramaService;
+            _organizacaoCore = organizacaoCore;
+        }
+
+        public int Count(string guidOrganizacao)
+        {
+            _validation.OrganizacaoFilled(guidOrganizacao);
+            _validation.OrganizacaoValid(guidOrganizacao);
+
+            Guid guid = new Guid(guidOrganizacao);
+
+            var count = _nivelsClassificacao.Where(pc => pc.GuidOrganizacao.Equals(guid))
+                                                  .Count();
+
+            return count;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            NivelClassificacao nivelClassificacao = SearchPersistence(id);
+
+            _validation.CanDelete(nivelClassificacao);
+
+            _nivelsClassificacao.Remove(nivelClassificacao);
+
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task<NivelClassificacaoModel> InsertAsync(NivelClassificacaoModel nivelClassificacaoModel)
+        {
+            _validation.BasicValid(nivelClassificacaoModel);
+
+            _validation.IdInsertValid(nivelClassificacaoModel.Id);
+
+            OrganogramaOrganizacao organogramaOrganizacaoPatriarca = await _organogramaService.SearchPatriarcaAsync(nivelClassificacaoModel.GuidOrganizacao);
+
+            OrganizacaoModel organizacaoPatriarca = _organizacaoCore.SearchAsync(organogramaOrganizacaoPatriarca.Guid.ToString());
+
+            nivelClassificacaoModel.Organizacao = organizacaoPatriarca;
+
+            NivelClassificacao nivelClassificacao = _mapper.Map<NivelClassificacao>(nivelClassificacaoModel);
+
+            await _nivelsClassificacao.AddAsync(nivelClassificacao);
+
+            await _unitOfWork.SaveAsync();
+
+            nivelClassificacaoModel = _mapper.Map<NivelClassificacaoModel>(nivelClassificacao);
+
+            return nivelClassificacaoModel;
+        }
+
+        public NivelClassificacaoModel Search(int id)
+        {
+            NivelClassificacao nivelClassificacao = SearchPersistence(id);
+
+            NivelClassificacaoModel nivelClassificacaoModel = _mapper.Map<NivelClassificacaoModel>(nivelClassificacao);
+
+            return nivelClassificacaoModel;
+        }
+
+        public List<NivelClassificacaoModel> Search(string guidOrganizacao, int page, int count)
+        {
+            _validation.OrganizacaoFilled(guidOrganizacao);
+            _validation.OrganizacaoValid(guidOrganizacao);
+
+            _validation.PaginationSearch(page, count);
+
+            Guid guid = new Guid(guidOrganizacao);
+            int skip = (page - 1) * count;
+
+            List<NivelClassificacao> nivelsClassificacao = _nivelsClassificacao.Where(pc => pc.GuidOrganizacao.Equals(guid))
+                                                                               .OrderBy(pc => pc.InicioVigencia.HasValue)
+                                                                               .ThenByDescending(pc => pc.InicioVigencia)
+                                                                               .ThenByDescending(pc => pc.Codigo)
+                                                                               .Skip(skip)
+                                                                               .Take(count)
+                                                                               .ToList()
+                                                                               ;
+            List<NivelClassificacaoModel> nivelsClassificacaoModel = _mapper.Map<List<NivelClassificacaoModel>>(nivelsClassificacao);
+
+            return nivelsClassificacaoModel;
+        }
+
+        public async Task UpdateAsync(NivelClassificacaoModel nivelClassificacaoModel)
+        {
+            _validation.Valid(nivelClassificacaoModel);
+
+            NivelClassificacao nivelClassificacao = SearchPersistence(nivelClassificacaoModel.Id);
+
+            _validation.CanUpdate(nivelClassificacao);
+
+            if (!nivelClassificacaoModel.GuidOrganizacao.ToUpper().Equals(nivelClassificacao.GuidOrganizacao.ToString().ToUpper()))
+            {
+                OrganogramaOrganizacao organogramaOrganizacaoPatriarca = await _organogramaService.SearchPatriarcaAsync(nivelClassificacaoModel.GuidOrganizacao);
+
+                OrganizacaoModel organizacaoPatriarca = _organizacaoCore.SearchAsync(organogramaOrganizacaoPatriarca.Guid.ToString());
+
+                if (nivelClassificacao.IdOrganizacao != organizacaoPatriarca.Id)
+                    nivelClassificacaoModel.Organizacao = organizacaoPatriarca;
+            }
+
+            _mapper.Map(nivelClassificacaoModel, nivelClassificacao);
+
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task UpdateFimVigenciaAsync(int id, DateTime fimVigencia)
+        {
+            NivelClassificacao nivelClassificacao = SearchPersistence(id);
+
+            _validation.FimVigenciaValid(fimVigencia, nivelClassificacao.InicioVigencia);
+
+            nivelClassificacao.FimVigencia = fimVigencia;
+
+            await _unitOfWork.SaveAsync();
+        }
+
+        private NivelClassificacao SearchPersistence(int id)
+        {
+            _validation.IdValid(id);
+
+            NivelClassificacao nivelClassificacao = _nivelsClassificacao.Where(pc => pc.Id == id)
+                                                                              .SingleOrDefault();
+
+            _validation.Found(nivelClassificacao);
+
+            return nivelClassificacao;
+        }
+    }
+}
---
+++ SCD/Business/Validation/NivelClassificacaoValidation.cs
@@ -0,0 +1,136 @@
+﻿using Prodest.Scd.Business.Common.Exceptions;
+using Prodest.Scd.Business.Model;
+using Prodest.Scd.Business.Validation.Common;
+using Prodest.Scd.Persistence.Base;
+using Prodest.Scd.Persistence.Model;
+using System;
+
+namespace Prodest.Scd.Business.Validation
+{
+    public class NivelClassificacaoValidation : CommonValidation
+    {
+        private IGenericRepository<NivelClassificacao>  _niveisClassificacao;
+
+        public NivelClassificacaoValidation(IScdRepositories repositories)
+        {
+            _niveisClassificacao = repositories.NiveisClassificacao;
+        }
+
+        #region Valid
+        internal void Valid(NivelClassificacaoModel nivelClassificacao)
+        {
+            BasicValid(nivelClassificacao);
+
+            IdValid(nivelClassificacao.Id);
+        }
+
+        internal void IdValid(int id)
+        {
+            if (id == default(int))
+                throw new ScdException("O id não pode ser nulo ou vazio.");
+        }
+
+        internal void IdInsertValid(int id)
+        {
+            if (id != default(int))
+                throw new ScdException("O id não deve ser preenchido.");
+        }
+
+        #endregion
+
+        #region Basic Valid
+        internal void BasicValid(NivelClassificacaoModel nivelClassificacao)
+        {
+            NotNull(nivelClassificacao);
+
+            Filled(nivelClassificacao);
+
+            OrganizacaoValid(nivelClassificacao.GuidOrganizacao);
+
+            PublicacaoValid(nivelClassificacao.Publicacao, nivelClassificacao.Aprovacao, nivelClassificacao.InicioVigencia);
+
+            FimVigenciaValid(nivelClassificacao.FimVigencia, nivelClassificacao.InicioVigencia);
+        }
+
+        private void NotNull(NivelClassificacaoModel nivelClassificacao)
+        {
+            if (nivelClassificacao == null)
+                throw new ScdException("O Nivel de Classificação não pode ser nulo.");
+        }
+
+        #region Filled
+        internal void Filled(NivelClassificacaoModel nivelClassificacao)
+        {
+            CodigoFilled(nivelClassificacao.Codigo);
+            DescricaoFilled(nivelClassificacao.Descricao);
+            OrganizacaoFilled(nivelClassificacao.GuidOrganizacao);
+        }
+
+        private void CodigoFilled(string codigo)
+        {
+            if (string.IsNullOrWhiteSpace(codigo) || string.IsNullOrWhiteSpace(codigo.Trim()))
+                throw new ScdException("O código não pode ser vazio ou nulo.");
+        }
+
+        private void DescricaoFilled(string descricao)
+        {
+            if (string.IsNullOrWhiteSpace(descricao) || string.IsNullOrWhiteSpace(descricao.Trim()))
+                throw new ScdException("A descrição não pode ser vazia ou nula.");
+        }
+
+        #endregion
+
+        private void PublicacaoValid(DateTime? publicacao, DateTime? aprovacao, DateTime? inicioVigencia)
+        {
+            if (publicacao.HasValue && !aprovacao.HasValue)
+                throw new ScdException("A data de aprovação não pode ser vazia ou nula quando existe uma data de publicação.");
+
+            if (publicacao.HasValue && !inicioVigencia.HasValue)
+                throw new ScdException("A data de início de vigência não pode ser vazia ou nula quando existe uma data de publicação.");
+
+            if ((publicacao.HasValue && aprovacao.HasValue) && (publicacao.Value < aprovacao.Value))
+                throw new ScdException("A data de publicação deve ser maior ou igual à data de aprovação.");
+        }
+
+        internal void FimVigenciaValid(DateTime? fimVigencia, DateTime? inicioVigencia)
+        {
+            if (fimVigencia.HasValue && !inicioVigencia.HasValue)
+                throw new ScdException("A data de início de vigência não pode ser vazia ou nula quando existe uma data de fim de vigência.");
+
+            if ((fimVigencia.HasValue && inicioVigencia.HasValue) && (fimVigencia.Value < inicioVigencia.Value))
+                throw new ScdException("A data de fim de vigência deve ser maior ou igual à data de início de vigência.");
+        }
+        #endregion
+
+        internal void Found(NivelClassificacao nivelClassificacao)
+        {
+            if (nivelClassificacao == null)
+                throw new ScdException("Nivel de Classificação não encontrado.");
+        }
+
+        internal void PaginationSearch(int page, int count)
+        {
+            if (page <= 0)
+                throw new ScdException("Página inválida.");
+
+            if (count <= 0)
+                throw new ScdException("Quantidade de rgistro por página inválida.");
+        }
+
+        internal void CanUpdate(NivelClassificacao nivelClassificacao)
+        {
+            if (nivelClassificacao.Publicacao.HasValue)
+                throw new ScdException("O Nivel de Classificação possui data de publicação e não pode ser atualizado.");
+        }
+
+        internal void CanDelete(NivelClassificacao nivelClassificacao)
+        {
+            if (nivelClassificacao.Publicacao.HasValue)
+                throw new ScdException("O Nivel de Classificação possui data de publicação e não pode ser excluído.");
+
+            if (nivelClassificacao.ItensNivelClassificacao != null && nivelClassificacao.ItensNivelClassificacao.Count > 0)
+                throw new ScdException("O Nivel de Classificação possui itens e não pode ser excluído.");
+        }
+
+    }
+}
