70fb41f13e681e6793d15b51266f2d0187f415a6
Fernando Fontoura de Almeida <ffalmeida@gmail.com>
2017-09-11 17:37:21 -0300

    Esqueleto do Item de Plano de Classificação.

---
+++ SCD/Business/Base/IItemPlanoClassificacaoCore.cs
@@ -0,0 +1,22 @@
+﻿using Prodest.Scd.Business.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Prodest.Scd.Business.Base
+{
+    public interface IItemPlanoClassificacaoCore
+    {
+        int Count(Guid guidOrganizacao);
+
+        Task DeleteAsync(int id);
+
+        Task<ItemPlanoClassificacaoModel> InsertAsync(ItemPlanoClassificacaoModel itemPlanoClassificacao);
+
+        ItemPlanoClassificacaoModel Search(int id);
+
+        List<ItemPlanoClassificacaoModel> Search(Guid guidOrganizacao, int page, int count);
+
+        Task UpdateAsync(ItemPlanoClassificacaoModel itemPlanoClassificacao);
+    }
+}
